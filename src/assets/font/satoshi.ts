f6a3cf9924e089e532daba20b1a5fc0045bebb71
Filip <filfer05@gmail.com>
2024-02-29 15:43:01 +0100

    initial

---
+++ src/assets/font/satoshi.ts
@@ -0,0 +1,11 @@
+import localFont from "next/font/local";
+
+export const satoshi = localFont({
+  src: [
+    { path: "./Satoshi-Light.woff2", weight: "300", style: "normal" },
+    { path: "./Satoshi-Regular.woff2", weight: "400", style: "normal" },
+    { path: "./Satoshi-Medium.woff2", weight: "500", style: "normal" },
+    { path: "./Satoshi-Bold.woff2", weight: "700", style: "normal" },
+  ],
+  variable: "--font-satoshi",
+});
