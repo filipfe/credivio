8e0d06b3469b5d023f232f366f6dfcb83c82b839
Filip <filfer05@gmail.com>
2024-03-06 22:25:00 +0100

    layout fix

--- src/app/(private)/incomes/page.tsx
+++ src/app/(private)/incomes/page.tsx
@@ -13,7 +13,7 @@ export default async function Page({
     searchParams
   );
   return (
-    <div className="px-12 pt-8 pb-24 flex flex-col h-full">
+    <div className="px-12 pt-8 pb-24 flex flex-col h-full gap-8">
       <IncomeTable
         title="Przychody"
         operations={incomes}
