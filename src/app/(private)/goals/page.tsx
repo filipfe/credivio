e13df57962c1420f9c73afd21c23b9f4481b895c
Filip <filfer05@gmail.com>
2024-04-22 10:48:17 +0200

    goals loading

--- src/app/(private)/goals/page.tsx
+++ src/app/(private)/goals/page.tsx
@@ -11,11 +11,13 @@ export default function Page() {
     <div className="px-10 pt-8 pb-24 flex flex-col h-full gap-8">
       <Suspense
         fallback={
-          <div className="grid grid-cols-4">
+          <div className="grid grid-cols-4 gap-6">
             <Skeleton className="h-48 rounded-md" />
             <Skeleton className="h-48 rounded-md" />
             <Skeleton className="h-48 rounded-md" />
             <Skeleton className="h-48 rounded-md" />
+            <Skeleton className="h-48 rounded-md col-span-2" />
+            <Skeleton className="h-48 rounded-md col-span-2" />
           </div>
         }
       >
