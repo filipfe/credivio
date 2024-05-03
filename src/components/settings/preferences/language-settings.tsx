d851f8a5be198fcbc8d8afd3ae72c82669da4e52
unknown <se6359@gmail.com>
2024-05-03 12:35:06 +0200

    preferences auto save

---
+++ src/components/settings/preferences/language-settings.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import UniversalSelect from "@/components/ui/universal-select";
+import { LANGUAGES } from "@/const";
+import { updatePreferences } from "@/lib/settings/actions";
+
+export default function LanguageSettings({ language }: { language: string }) {
+  return (
+    <div>
+      <h2 className="text-lg font-bold">Język</h2>
+      <div className="flex text-nowrap items-center justify-between">
+        Wybierz język
+        <UniversalSelect
+          className="w-32"
+          size="sm"
+          name="language"
+          aria-label="Język"
+          defaultSelectedKeys={[language]}
+          elements={LANGUAGES}
+          onChange={(e) => updatePreferences("language", e.target.value)}
+        />
+      </div>
+    </div>
+  );
+}
