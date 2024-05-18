a69ce67f72aea7e2716a3a866552c667bdb10ccd
Filip <filfer05@gmail.com>
2024-05-18 16:23:09 +0200

    supabase init

---
+++ marketing/src/components/landing/scroll-carousel.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function ScrollCarousel({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => {
+      console.log(window, window.scrollY);
+      setOffset(window.scrollY);
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      style={{ transform: `translateX(${offset / 4}px)` }}
+      className="flex items-center min-w-max gap-2"
+    >
+      {children}
+    </div>
+  );
+}
