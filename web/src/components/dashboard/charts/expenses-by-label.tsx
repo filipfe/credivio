7a3bc10d4eafcb0b1df2c2f1558282ba5e126504
mgvy <se6359@gmail.com>
2024-05-22 18:09:57 +0200

    .

--- web/src/components/dashboard/charts/expenses-by-label.tsx
+++ web/src/components/dashboard/charts/expenses-by-label.tsx
@@ -1,4 +1,6 @@
 import BarChart from "@/components/ui/charts/bar-chart";
+import UniversalSelect from "@/components/ui/universal-select";
+import { CURRENCIES } from "@/const";
 import { getChartLabels } from "@/lib/operation/actions";
 
 export default async function ExpensesByLabel({
@@ -9,7 +11,17 @@ export default async function ExpensesByLabel({
   const { results: chartLabels } = await getChartLabels(defaultCurrency);
   return (
     <div className="xl:col-span-3 bg-white sm:rounded-md px-6 py-8">
-      <h3 className="mb-4 text-center">Wydatki wg Etykieta</h3>
+      <div className="flex justify-between items-center ml-[8px] mr-[36px] mb-4">
+        <h3>Wydatki wg Etykieta</h3>
+        <UniversalSelect
+          className="w-20"
+          size="sm"
+          name="currency"
+          aria-label="Waluta"
+          defaultSelectedKeys={[defaultCurrency]}
+          elements={CURRENCIES}
+        />
+      </div>
       <BarChart data={chartLabels} defaultCurrency={defaultCurrency} />
     </div>
   );
--- web/src/components/dashboard/charts/operations-by-month.tsx
+++ web/src/components/dashboard/charts/operations-by-month.tsx
@@ -1,4 +1,6 @@
 import AreaChart from "@/components/ui/charts/line-chart";
+import UniversalSelect from "@/components/ui/universal-select";
+import { CURRENCIES } from "@/const";
 import { getDailyTotalAmount } from "@/lib/operation/actions";
 
 export default async function OperationsByMonth({
@@ -15,14 +17,23 @@ export default async function OperationsByMonth({
 
   return (
     <div className="xl:col-span-3 bg-white sm:rounded-md px-6 py-8 h-full">
-      <h3 className="mb-4 text-center">
-        {type === "budget"
-          ? "Budżet "
-          : type === "income"
-          ? "Przychody "
-          : "Wydatki "}
-        wg 30 dni
-      </h3>
+      {type === "budget" ? (
+        <div className="flex justify-between items-center ml-[12px] mr-[36px] mb-4">
+          <h3>Budżet wg 30 dni</h3>
+          <UniversalSelect
+            className="w-20"
+            size="sm"
+            name="currency"
+            aria-label="Waluta"
+            defaultSelectedKeys={[defaultCurrency]}
+            elements={CURRENCIES}
+          />
+        </div>
+      ) : (
+        <h3 className="mb-4 text-center">
+          {type === "income" ? "Przychody" : "Wydatki"} wg 30 dni
+        </h3>
+      )}
       <AreaChart
         data={dailyTotalAmount}
         defaultCurrency={defaultCurrency}
