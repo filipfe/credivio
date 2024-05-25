d4d8b5d201504ebfcea724488bddf9be646647fc
mgvy <se6359@gmail.com>
2024-05-26 00:49:06 +0200

    latest operations loader

--- web/src/components/dashboard/operations/ref.tsx
+++ web/src/components/dashboard/operations/ref.tsx
@@ -1,28 +1,44 @@
 import numberFormat from "@/utils/formatters/currency";
+import { Skeleton, cn } from "@nextui-org/react";
 
 export default function Operation(props: LatestOperation) {
   const operation = props;
 
   return (
-    <div className="bg-white rounded-lg py-4 px-6 flex flex-col justify-between relative opacity-100 min-w-60">
-      <div className="flex flex-col gap-3">
-        <div className="flex items-center justify-between">
-          <h3 className="text-lg line-clamp-1">{operation.title}</h3>
-          <small className="text-neutral-500">
-            {new Date(operation.issued_at).toLocaleDateString()}
-          </small>
-        </div>
-        <div
-          className={`${
-            operation.type === "income"
-              ? "bg-success-light text-success"
-              : "bg-danger-light text-danger"
-          } rounded-full px-1 py-0.5 text-2xl font-bold text-center`}
-        >
-          {(operation.type === "income" ? "+" : "-") +
-            numberFormat(operation.currency, operation.amount)}
-        </div>
+    <div className="bg-white rounded-lg py-4 px-6 flex flex-col gap-3 min-w-60">
+      <div className="flex items-center justify-between">
+        <h3 className="text-lg line-clamp-1">{operation.title}</h3>
+        <small className="text-neutral-500">
+          {new Date(operation.issued_at).toLocaleDateString()}
+        </small>
       </div>
+      <div
+        className={`${
+          operation.type === "income"
+            ? "bg-success-light text-success"
+            : "bg-danger-light text-danger"
+        } rounded-full px-1 py-0.5 text-2xl font-bold text-center`}
+      >
+        {(operation.type === "income" ? "+" : "-") +
+          numberFormat(operation.currency, operation.amount)}
+      </div>
+    </div>
+  );
+}
+
+export function OperationLoader({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        "bg-white rounded-lg py-4 px-6 flex flex-col gap-3",
+        className
+      )}
+    >
+      <div className="flex items-center justify-between">
+        <Skeleton className="h-6 w-24 rounded-full" />
+        <Skeleton className="h-5 w-16 rounded-full" />
+      </div>
+      <Skeleton className="px-1 py-0.5 h-9 w-48 rounded-full" />
     </div>
   );
 }
