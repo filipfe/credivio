2a4436aa8068429a45198167cfa3ab62f9d2d238
mgvy <se6359@gmail.com>
2024-05-25 20:42:07 +0200

    bar chart currency select

--- web/src/components/dashboard/charts/expenses-by-label.tsx
+++ web/src/components/dashboard/charts/expenses-by-label.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import BarChart from "@/components/ui/charts/bar-chart";
 import UniversalSelect from "@/components/ui/universal-select";
 import { CURRENCIES } from "@/const";
 import { getChartLabels } from "@/lib/operation/actions";
+import { useEffect, useState } from "react";
 
 export default async function ExpensesByLabel({
   defaultCurrency,
 }: {
   defaultCurrency: string;
 }) {
-  const { results: chartLabels } = await getChartLabels(defaultCurrency);
+  const [chartLabels, setChartLabels] = useState<ChartLabel[]>([]);
+  const [currency, setCurrency] = useState<string>(defaultCurrency);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { results } = await getChartLabels(currency);
+      setChartLabels(results);
+    }
+    fetchData();
+  }, [currency]);
+
   return (
     <div className="xl:col-span-3 bg-white sm:rounded-md px-6 py-8">
       <div className="flex justify-between items-center ml-[8px] mr-[36px] mb-4">
@@ -18,11 +31,12 @@ export default async function ExpensesByLabel({
           size="sm"
           name="currency"
           aria-label="Waluta"
-          defaultSelectedKeys={[defaultCurrency]}
+          defaultSelectedKeys={[currency]}
           elements={CURRENCIES}
+          onChange={(e) => setCurrency(e.target.value)}
         />
       </div>
-      <BarChart data={chartLabels} defaultCurrency={defaultCurrency} />
+      <BarChart data={chartLabels} currency={currency} />
     </div>
   );
 }
