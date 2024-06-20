66e4a5032bd7d0dadca1afe888d2bbe6f95d67a4
Filip <filfer05@gmail.com>
2024-06-20 19:27:18 +0200

    auth flow

--- web/src/app/(public)/sign-in/page.tsx
+++ web/src/app/(public)/sign-in/page.tsx
@@ -1,10 +1,40 @@
 import Form from "@/components/auth/form";
+import { Input } from "@nextui-org/react";
 
 export default function Page() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-light">
-      <div className="bg-white rounded-lg px-10 py-8 w-full max-w-[28rem]">
-        <Form />
+      <div className="bg-white rounded-lg px-10 py-8 w-full max-w-[28rem] border border-primary/10">
+        <Form>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col items-center text-center gap-2 mb-4">
+              <div className="bg-light rounded-md h-16 w-16 mb-4 border border-primary/10"></div>
+              <h1 className="text-2xl font-medium">Witaj z powrotem!</h1>
+              <p className="text-sm">Zaloguj się, aby kontynuować</p>
+            </div>
+            <Input
+              classNames={{ inputWrapper: "!bg-light" }}
+              name="email"
+              label="Email"
+              type="email"
+              placeholder="example@mail.com"
+              labelPlacement="outside"
+              isRequired
+              required
+              autoComplete="off"
+            />
+            <Input
+              classNames={{ inputWrapper: "!bg-light" }}
+              name="password"
+              label="Hasło"
+              type="password"
+              placeholder="**********"
+              labelPlacement="outside"
+              isRequired
+              required
+            />
+          </div>
+        </Form>
       </div>
     </div>
   );
--- web/src/lib/auth/actions.ts
+++ web/src/lib/auth/actions.ts
@@ -1,24 +1,59 @@
 "use server";
 
 import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-export async function signInWithEmail(formData: FormData) {
+export async function signUp(formData: FormData) {
+  const first_name = formData.get("first-name")?.toString();
+  const last_name = formData.get("last-name")?.toString();
+  const email = formData.get("email")?.toString() || "";
+  const password = formData.get("password")?.toString() || "";
+
   const supabase = createClient();
 
-  const { error } = await supabase.auth.signInWithOtp({
-    email: formData.get("email")?.toString() || "",
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
     options: {
-      shouldCreateUser: true,
       emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/confirm`,
       data: {
         language: "Polski",
         currency: "PLN",
+        first_name,
+        last_name,
       },
     },
   });
 
-  return error;
+  if (error) {
+    return {
+      error: error.message,
+    };
+  }
+
+  return {};
+}
+
+export async function signIn(formData: FormData) {
+  const email = formData.get("email")?.toString() || "";
+  const password = formData.get("password")?.toString() || "";
+
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return {
+      error: error.message,
+    };
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
 }
 
 export async function signOut() {
