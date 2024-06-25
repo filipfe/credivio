6d23f04923ef47f16098d43d6d3b529e7b5833a2
Filip <filfer05@gmail.com>
2024-06-25 23:11:54 +0200

    fix latest operations

--- web/src/components/ui/horizontal-scroll.tsx
+++ web/src/components/ui/horizontal-scroll.tsx
@@ -4,13 +4,17 @@ import { MenuContext } from "@/app/(private)/providers";
 import { ScrollShadow, cn } from "@nextui-org/react";
 import { useContext } from "react";
 
+type Props = {
+  children: React.ReactNode[];
+  className?: string;
+  innerClassName?: string;
+};
+
 export default function HorizontalScroll({
   children,
   className,
-}: {
-  children: React.ReactNode[];
-  className?: string;
-}) {
+  innerClassName,
+}: Props) {
   const { isMenuHidden } = useContext(MenuContext);
   return (
     <ScrollShadow
@@ -19,12 +23,14 @@ export default function HorizontalScroll({
       className={cn(
         "w-full",
         isMenuHidden.desktop
-          ? "max-w-[calc(100vw-48px)] sm:max-w-[calc(100vw-176px)]"
-          : "max-w-[calc(100vw-48px)] sm:max-w-[calc(100vw-416px)]",
+          ? "max-w-[100vw] sm:max-w-[calc(100vw-176px)]"
+          : "max-w-[100vw] sm:max-w-[calc(100vw-416px)]",
         className
       )}
     >
-      <div className="flex items-stretch gap-6 min-w-max">{children}</div>
+      <div className={cn("flex items-stretch gap-6 min-w-max", innerClassName)}>
+        {children}
+      </div>
     </ScrollShadow>
   );
 }
