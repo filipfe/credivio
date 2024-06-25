9e45a89387bce1ce872c0df63ef0977f28c1820f
Filip <filfer05@gmail.com>
2024-06-25 18:23:47 +0200

    fix seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -3,7 +3,7 @@ insert into "storage"."buckets"
 ('docs', 'docs', false, 10485760, '{image/jpeg, image/png, application/pdf}');
 
 insert into "public"."services"
-(name, href, price, title, decription) values
+(name, href, price, title, description) values
 ('stocks', '/stocks', 14.99, 'Akcje', 'Usługa zarządzania akcjami umożliwia efektywne zarządzanie finansami, oferując narzędzia do monitorowania inwestycji, analizowania rynku i podejmowania świadomych decyzji inwestycyjnych. Dzięki niej użytkownicy mogą śledzić wartość swoich portfeli, otrzymywać powiadomienia o istotnych zmianach rynkowych oraz korzystać z raportów i analiz dostosowanych do ich indywidualnych potrzeb.')
 
   -- insert into auth.users (instance_id, id, aud, role, email, encrypted_password, raw_app_meta_data, raw_user_meta_data, email_confirmed_at, created_at)
