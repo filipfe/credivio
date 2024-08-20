b738f8d2a3f9dde129e4bfa12fd148d25e13a37c
Filip <filfer05@gmail.com>
2024-08-20 13:43:44 +0200

    stats

--- web/src/components/dashboard/stats/list.tsx
+++ web/src/components/dashboard/stats/list.tsx
@@ -28,7 +28,7 @@ export default async function StatsList({
         title="Przychody"
         currency={defaultCurrency}
         description=""
-        amount={incomes.amount}
+        amount={incomes}
         cta={
           <Link href="/incomes/add">
             <Button
@@ -47,7 +47,7 @@ export default async function StatsList({
         title="Wydatki"
         currency={defaultCurrency}
         description=""
-        amount={expenses.amount}
+        amount={expenses}
         cta={
           <Link href="/expenses/add">
             <Button
@@ -66,7 +66,7 @@ export default async function StatsList({
         title="Bilans"
         currency={defaultCurrency}
         description=""
-        amount={balance.amount}
+        amount={balance}
       />
     </Fragment>
   );
