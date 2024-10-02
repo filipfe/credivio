e4d7e2d1b8c2ae27d081cfa96f4cb36a94eec9b3
Filip <filfer05@gmail.com>
2024-10-03 01:48:37 +0200

    test

--- web/src/app/(private)/ai-assistant/page.tsx
+++ web/src/app/(private)/ai-assistant/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
   return (
     <div className="sm:px-10 py-4 sm:py-8 flex flex-col h-full gap-4 sm:gap-6 xl:grid grid-cols-2">
       <AIAssistantProvider>
-        <Chat />
+        {/* <Chat /> */}
         <Block
           title="Zbuduj kontekst"
           description="Wybierz informacje, które mają być przetworzone przez asystenta"
